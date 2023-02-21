72b1643cc99bba045f2e12fa4f6f947a8c7427a5
sai <prasathsaai@gmail.com>
2023-02-21 19:07:19 +0530

    update6

--- launch-ec2.sh
+++ launch-ec2.sh
@@ -23,7 +23,7 @@ create-server() {
     echo "Spot Instance $COMPONENT is ready: "
     echo "Creating Route53 Record . . . . :"
 
-    sed -e "s/PRIVATEIP/${PRIVATE_IP}/" -e "s/COMPONENT/${COMPONENT}${ENV}/" r53.json  >/tmp/record.json 
+    sed -e "s/PRIVATEIP/${PRIVATE_IP}/" -e "s/COMPONENT/${COMPONENT}-${ENV}/" r53.json  >/tmp/record.json 
     aws route53 change-resource-record-sets --hosted-zone-id ${ZONEID} --change-batch file:///tmp/record.json | jq 
 }
 
@@ -35,4 +35,4 @@ if [ "$1" == "all" ] ; then
      done
 else 
      create-server
-fi 
+fi  
