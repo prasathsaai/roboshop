30674fff861f6324f81c03428c7d1bf0b9297b17
sai <prasathsaai@gmail.com>
2023-02-25 17:37:38 +0530

    adding per1

--- launch-ec2.sh
+++ launch-ec2.sh
@@ -23,8 +23,7 @@ create-server() {
     echo "Spot Instance $COMPONENT is ready: "
     echo "Creating Route53 Record . . . . :"
 
-    sed -e "s/PRIVATEIP/${PRIVATE_IP}/" -e "s/COMPONENT/${COMPONENT}-${ENV}/" r53.json  >/tmp/record.json 
-    chmod +x /tmp/record.json 
+    sudo sed -e "s/PRIVATEIP/${PRIVATE_IP}/" -e "s/COMPONENT/${COMPONENT}-${ENV}/" r53.json  >/tmp/record.json  
     aws route53 change-resource-record-sets --hosted-zone-id ${ZONEID} --change-batch file:///tmp/record.json | jq 
 }
 
